1e7e3ada9a4bae70890560ed6d43ad467bb37ffa
Thejesh P R <thejeshpr@gmail.com>
2021-11-06 17:22:02 +0530

    gui changes and custom tag

--- wru/templatetags/custom_tags.py
+++ wru/templatetags/custom_tags.py
@@ -15,3 +15,13 @@ def get_entry_icon():
 @register.simple_tag
 def get_feeling_icon():
     return "las la-grin-squint"
+
+
+@register.simple_tag
+def get_tag_icon():
+    return "las la-tags"
+
+
+@register.simple_tag
+def get_created_at_icon():
+    return "las la-calendar-day"
